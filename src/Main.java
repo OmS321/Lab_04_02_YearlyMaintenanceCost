3d78b122f05b3c21d3a92194f89037b3dfa3b34f
omsat <omsivasundar2010@gmail.com>
2025-06-03 10:59:48 -0400

    Here is the code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springMain = 250.99;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Spring maintenance: $" + springMain);
+
+
+        double summerMain = 300.99;
+
+        System.out.println("Summer maintenance: $" + summerMain);
+
+
+        double fallMain = 400.99;
+
+        System.out.println("Fall maintenance: $" + fallMain);
+
+
+        double winterMain = 250.99;
+
+        System.out.println("Winter maintenance: $" + winterMain);
+
+
+        double totalMain = springMain + summerMain + fallMain + winterMain;
+
+        System.out.println("TOTAL MAINTENANCE: $" + totalMain);
     }
 }
